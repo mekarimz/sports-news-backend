e1ce2d6f157d78184f47e5cedb5103bd1fc4833b
NAGARAJU <nagaraju@NAGARAJUs-iMac.local>
2025-08-01 18:58:49 +0530

    Bind to 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def get_news():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
